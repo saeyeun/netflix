9d3e2040643ddf4e2e092905a7680fc8ccadc207
saeyeun <uysy1200@gmail.com>
2023-10-10 21:57:33 +0900

    제이쿼리 변경, 푸터 문구 추가

--- js/script.js
+++ js/script.js
@@ -1,31 +1,6 @@
-// $(document).ready(function(){
-//   $('.question').click(function(){
-//     $(this).next().slideToggle();
-//     $(this).next().siblings('.answer').slideUp();
-
-//   })
-
-// })
-var list = document.getElementsByClassName('question_list');
-var question = document.getElementsByClassName('question_headline');
-var answer = document.getElementsByClassName('question_txt');
-
-for (var i = 0; i < question.length; i++) {
-  question[i].addEventListener('click', function (e) {
-    for (var a = 0; a < list.length; a++) {
-      list[a].classList.remove('active');
-      e.target.parentNode.classList.add('active');
-      activateBody();
-    }
+$(document).ready(function(){
+  $('.question_headline').click(function(){
+    $(this).next().slideToggle();
+    $(this).next().siblings('.question_txt').slideUp();
   })
-}
-
-function activateBody() {
-  for (var b = 0; b < answer.length; b++) {
-    answer[b].style.display = 'none';
-  }
-  var activePanel = document.querySelector('.question_list.active .question_txt');
-  activePanel.style.display = "block";
-}
-
-activateBody();
+})
